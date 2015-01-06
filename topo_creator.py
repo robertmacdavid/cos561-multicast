30b0855d758a10b323510d6c0ed0aadff826a4c1
Robert MacDavid <robertmacdavid@gmail.com>
2015-01-05 21:48:17 -0800

    fixed topo creator

--- topo_creator.py
+++ topo_creator.py
@@ -20,8 +20,6 @@ args = parser.parse_args()
     
 class SingleSwitchTopo(Topo):
     "Single switch connected to n hosts."
-    def __init__(self, n=4, **opts):
-        self.build(n)
     def build(self, n=2):
         switch = self.addSwitch('s1')
         # Python's range(N) generates 0..N-1
@@ -32,8 +30,6 @@ class SingleSwitchTopo(Topo):
 
 class CycleTopo(Topo):
     "Cycle of switches with two hosts per switch."
-    def __init__(self, n=4, **opts):
-        self.build(n)
     def build(self, n=4):
         # build the cycle nodes
         for i in range(n):
@@ -51,7 +47,8 @@ class CycleTopo(Topo):
 
 def simpleTest():
     "Create and test a simple network"
-    topo = CycleTopo(n=args.node_count)
+    topo = CycleTopo()
+    topo.build(n=args.node_count)
     net = Mininet(topo)
     net.start()
     print "Dumping host connections"
