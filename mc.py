8316cf44991bd46a7b403f3c9a43c752e15a493e
Robert MacDavid <robertmacdavid@gmail.com>
2015-01-07 08:25:40 -0800

    maybe fixed mc

--- mc.py
+++ mc.py
@@ -1,25 +1,30 @@
-
 # some imports
 import socket, sys
 from struct import *
 
+## this shuts up scapy whining about IPv6 errors
 import logging
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 from scapy.all import *
+##
 
-# this block gets the host's non-loopback adapter's IPv4 address
+## this block gets the host's non-loopback adapter's IPv4 address
 from netifaces import AF_INET, AF_INET6, AF_LINK, AF_PACKET, AF_BRIDGE
 import netifaces as ni
 ifaces = ni.interfaces()
 ifaces = [iface for iface in ifaces if iface != "lo"]
 host_iface = ifaces[0]
 host_address = ni.ifaddresses(host_iface)[2][0]['addr']
+##
 
+## GLOBAL VARS HERE ##
 commIP = "255.0.0.0"
 protos = {'create':250, 'destroy':251, 'join':252, 'leave':253}
+failureIP = "0.0.0.0"
+######################
 
 
-# check input for some correctness
+## check input for some correctness
 def usage():
     print 'Usage: python %s <Action> <GroupIP>' % (sys.argv[0])
     print 'Actions: create, destroy, join, leave, msg, sniff. (create/sniff takes no GroupIP)'
@@ -33,12 +38,12 @@ if sys.argv[1][0] == 'c' or sys.argv[1][0] == 's':
         usage()
 elif len(sys.argv) != 3:
     usage()
-# done checking
+## done checking
 
 
 
 
-# send a packet and return the response
+## send a packet and return the response
 def contactControl(host, groupID, protocol):
     p = IP()/UDP()
     p.src = host
@@ -50,86 +55,100 @@ def contactControl(host, groupID, protocol):
         print "No response received."
         exit()
     return response
+##
 
-# response printing duh
+
+## response printing duh
 def printResponse(resp):
     print "Got response: Protocol %d, Source %s" %(resp.proto, resp.src)
     # hexdump(resp)
     # uncomment the above line for a hex dump of the response packet
+##
 
 
+##### Option Functions ######
 
-# handle a request to create a group
+## handle a request to create a group
 def create(host, groupID = commIP):
     print "Create a group..."
     resp = contactControl(host, groupID, protos['create'])
     printResponse(resp)
+##
 
-
-# handle a request to destroy a group
+## handle a request to destroy a group
 def destroy(host, groupID):
     print "Destroy", groupID, "..."
     resp = contactControl(host, groupID, protos['destroy'])
     printResponse(resp)
+##
 
-# handle a request to join a group
+## handle a request to join a group
 def join(host, groupID):
     print "Join", groupID, "..."
     resp = contactControl(host, groupID, protos['join'])
     printResponse(resp)
     group = resp.dst
-    if group != "0.0.0.0":
+    if group != failureIP:
         print "Interpreted response as successful join."
         print "---Sniffing %s. Ctrl+C to stop---" % host_iface
         sniff(iface = host_iface, prn=lambda x: x.summary())
 
-# handle a request to leave a group
+## handle a request to leave a group
 def leave(host, groupID):
     print "Leave", groupID, "..."
     resp = contactControl(host, groupID, protos['leave'])
     printResponse(resp)
+##
 
 def sniffy(host, groupID = None):
     print "---Sniffing %s. Ctrl+C to stop---" % host_iface
     sniff(iface = host_iface, prn=lambda x:x.summary())
+##
+
+#########           ###########
 
 
 
-
+## Main Script
 
 actions = {'c':create, 'd':destroy, 'j':join, 'l':leave, 's':sniffy}
 
 action = sys.argv[1][0]
 
 
-# special handling because create takes no groupID
+## special handling because create/sniff take no groupID arg
 group = commIP
 if action != 'c' and action != 's':
     group = sys.argv[2]
+##
 
 
-# run the desired action
+## Run the desired action's function
 if action != 'm':
     if action != 's':
         print "Sending request to",
     actions[action](host_address, group)
-# handle data messages in a special way cause the payload and no response
+## Messages to the group are handled differently
 else:
-    #data = raw_input("Type the UDP Payload: ")
-    #print "Sending data '%s' to" % data, group
-    print "Sending a UDP packet to", group
+    # lets you type the payload so you can recognize it in a packet dump
+    data = raw_input("Type the UDP Payload: ")
+    print "Sending UDP data '%s' to" % data, group
     
-    p = IP(src = host_address, dst = group)/fuzz(UDP())
+    ## fuzzing the UDP packet fixes some crap with unset header fields
+    ## it shouldnt be necessary because SCAPY IS SUPPOSED TO FIX THEM ON SEND
+    p = IP(src = host_address, dst = group)/fuzz(UDP()/(data))
 
-    responses, unans = sr(p, verbose=0, timeout = 1)
+    send(p, verbose=0)
+    print "Sniffing for responses for 2 seconds.."
+    responses = sniff(iface = host_iface, timeout = 2)
     if len(responses) == 0:
         print "No responses to list."
         exit()
-    print "--List of responses--"
-    #responses.summary()
+    print "--List of incoming packets--"
     for resp in responses:
-        print "Source: %s, Dest %s, Protocol %d" %(resp[1].src, resp[1].dst, resp[1].proto)
-    print "---------------------"
+        #print "Source: %s, Dest %s, Protocol %d" %(resp[1].src, resp[1].dst, resp[1].proto)
+        print resp.summary()
+    print "----------------------------"
                              
 
 
