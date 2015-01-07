2300b66d2d408911b9830739e1a7f32c2357f3f6
Robert MacDavid <robertmacdavid@gmail.com>
2015-01-07 00:33:47 -0800

    made mc.py better

--- mc.py
+++ mc.py
@@ -7,7 +7,7 @@ import logging
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 from scapy.all import *
 
-# things to get the host's non-loopback adapter IPv4 address
+# this block gets the host's non-loopback adapter's IPv4 address
 from netifaces import AF_INET, AF_INET6, AF_LINK, AF_PACKET, AF_BRIDGE
 import netifaces as ni
 ifaces = ni.interfaces()
@@ -21,7 +21,7 @@ protos = {'create':250, 'destroy':251, 'join':252, 'leave':253}
 # check input for some correctness
 def usage():
     print 'Usage: python %s <Action> <GroupIP>' % (sys.argv[0])
-    print 'Actions: create, destroy, join, leave (create takes no GroupIP)'
+    print 'Actions: create, destroy, join, leave, msg. (create takes no GroupIP)'
     sys.exit()
 
 if len(sys.argv) < 2:
@@ -32,6 +32,7 @@ if sys.argv[1][0] == 'c':
         usage()
 elif len(sys.argv) != 3:
     usage()
+# done checking
 
 
 
@@ -49,35 +50,39 @@ def contactControl(host, groupID, protocol):
         exit()
     return response
 
+# response printing duh
 def printResponse(resp):
     print "Got response: Protocol %d, Source %s" %(resp.proto, resp.src)
+    # hexdump(resp)
+    # uncomment the above line for a hex dump of the response packet
 
 
 
 # handle a request to create a group
 def create(host, groupID = commIP):
-    print "Creating..."
+    print "Create a group..."
     resp = contactControl(host, groupID, protos['create'])
     printResponse(resp)
 
 
 # handle a request to destroy a group
 def destroy(host, groupID):
-    print "Destroying..."
+    print "Destroy", groupID, "..."
     resp = contactControl(host, groupID, protos['destroy'])
     printResponse(resp)
 
 # handle a request to join a group
 def join(host, groupID):
-    print "Joining..."
+    print "Join", groupID, "..."
     resp = contactControl(host, groupID, protos['join'])
     printResponse(resp)
 
 # handle a request to leave a group
 def leave(host, groupID):
-    print "Leaving..."
+    print "Leave", groupID, "..."
     resp = contactControl(host, groupID, protos['leave'])
     printResponse(resp)
+
 
 
 
@@ -93,7 +98,17 @@ if action != 'c':
 
 
 # run the desired action
-actions[action](host_address, group)
+if action != 'm':
+    print "Sending request to",
+    actions[action](host_address, group)
+# handle data messages in a special way cause the payload and no response
+else:
+    data = raw_input("Type the UDP Payload: ")
+    print "Sending data '%s' to" % data, group
+    
+    send( IP(src=host_address, dst=group)/UDP()/(data) )
+                             
+                             
 
 """
 
