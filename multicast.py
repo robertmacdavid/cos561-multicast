7ad3cb7f1719cccc6519382cbb32d4d7875217fd
supersergiy <sergiy.popovich@gmail.com>
2015-01-08 23:39:47 -0800

    responses almost work

--- multicast.py
+++ multicast.py
@@ -58,27 +58,27 @@ def multicast_reply(network, pkt, groupID):
    srcmac = pkt['srcmac']
    # dstip  = pkt['dstip']
    dstmac = pkt['dstmac']
-    
-    
-    
+
+
+
    rp = Packet()
    rp = rp.modify(protocol = 17) # 17 is UDP
    rp = rp.modify(ethtype = 0x0800) # IP type
    rp = rp.modify(switch=switch)
-    
+
    rp = rp.modify(inport=-1)
    rp = rp.modify(outport=inport)
-    
+
    # groupID conveyed through the source IP
    # src and dst MACs are flipped for a response
-   rp = rp.modify(srcip=groupID)
+   rp = rp.modify(srcip=group_ip(groupID))
    rp = rp.modify(srcmac=dstmac)
-    
+
    rp = rp.modify(dstip=srcip)
    rp = rp.modify(dstmac=srcmac)
-    
+
    rp = rp.modify(raw='')
-    
+
    network.inject_packet(rp)
 
 
